c10f2ffdab0a56e1652db8366d8d4d68f409bdf6
Yiyi Sun <yiyisun@gmail.com>
2021-05-30 09:26:13 -0400

    add toReact.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import { useState } from 'react';
 import { Component } from 'apprun/esm/component';
+import toReact from './toReact';
+class MyComponent extends Component {
+  state = 0;
+  update = {
+    '-1': state => state - 1,
+    '+1': state => state + 1,
+  }
+  view = (state) => {
+    return <div>
+      <h1>{state}</h1>
+      <button onClick={() => this.run('-1')}>-1</button>
+      <button onClick={() => this.run('+1')}>+1</button>
+    </div>;
+  }
 
-const model = 0;
-
-const update = {
-  '-1': state => state - 1,
-  '+1': state => state + 1,
 }
 
-function view (state) {
-  return <div>
-    <h1>{state}</h1>
-    <button onClick={() => this.run('-1')}>-1</button>
-    <button onClick={() => this.run('+1')}>+1</button>
-  </div>;
-}
-
-function App() {
-  const [state, setState] = useState(model);
-  const [component] = useState(new Component(model, setState, update).mount());
-  return <>{view.bind(component)(state)}</>;
-}
-
+const App = toReact(MyComponent);
 export default App;
